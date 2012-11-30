86f2149a4379857c1c08d9efcfeb866798545151
zainul.franciscus@gmail.com <zainul.franciscus@gmail.com@8c8cbd4e-df39-adea-edf0-913d4a500e9e>
2012-11-30 21:01:40 +0000

    add search by ingredient in the controller

--- RecipeProject/src/test/java/com/safe/stack/controller/HomeControllerTest.java
+++ RecipeProject/src/test/java/com/safe/stack/controller/HomeControllerTest.java
@@ -63,4 +63,40 @@ public class HomeControllerTest extends AbstractControllerTest {
 
 	}
 
+	@Test
+	public void testSearchRecipes() {
+
+		String ingredient = "spinach beef";
+
+		List<String> ingredients = new ArrayList<String>();
+		ingredients.add("spinach");
+		ingredients.add("beef");
+
+		Recipe spinachRecipe = new Recipe();
+		spinachRecipe.setIngredient("spinach");
+
+		Recipe beefRecipe = new Recipe();
+		beefRecipe.setIngredient("beef");
+
+		List<Recipe> recipes = new ArrayList<Recipe>();
+		recipes.add(spinachRecipe);
+		recipes.add(beefRecipe);
+
+		RecipeService recipeService = mock(RecipeService.class);
+		when(recipeService.findByIngredients(ingredients)).thenReturn(recipes);
+
+		HomeController homeController = new HomeController();
+		ReflectionTestUtils.setField(homeController, "recipeService",
+				recipeService);
+		
+		ExtendedModelMap uiModel = new ExtendedModelMap();
+		String result = homeController.searchRecipes(ingredient, uiModel);
+		
+		assertNotNull(result);
+		assertEquals(result, "list");
+		
+		assertEquals(recipes, uiModel.get("recipes"));
+
+	}
+
 }
