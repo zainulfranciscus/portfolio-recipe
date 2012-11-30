d9237799097dc0fd4481acaca135d90fbc0356be
zainul.franciscus@gmail.com <zainul.franciscus@gmail.com@8c8cbd4e-df39-adea-edf0-913d4a500e9e>
2012-11-30 21:01:40 +0000

    add search by ingredient in the controller

--- trunk/RecipeProject/src/main/java/com/safe/stack/web/controller/HomeController.java
+++ trunk/RecipeProject/src/main/java/com/safe/stack/web/controller/HomeController.java
@@ -1,11 +1,15 @@
 package com.safe.stack.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.safe.stack.service.RecipeService;
 
@@ -28,6 +32,21 @@ public class HomeController {
 	{
 		uiModel.addAttribute("recipe", recipeService.findRecipe(id));
 		return "recipe";
+	}
+	
+	@RequestMapping(value= "/searchRecipeByIngredient", method = RequestMethod.POST)
+	public String searchRecipes(@RequestParam("ingredient") String ingredient, Model uiModel)
+	{
+		String[] tokens = ingredient.trim().split("\\s+");
+		List<String> ingredients = new ArrayList<String>();
+		
+		for (String t : tokens)
+		{
+			ingredients.add(t);
+		}
+		
+		uiModel.addAttribute("recipes", recipeService.findByIngredients(ingredients));
+		return "list";
 	}
 
 }
