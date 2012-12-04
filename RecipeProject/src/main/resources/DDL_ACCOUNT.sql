cbdc742b25db46a7296799919a28c73eb963d9ab
zainul.franciscus@gmail.com <zainul.franciscus@gmail.com@8c8cbd4e-df39-adea-edf0-913d4a500e9e>
2012-12-04 01:56:02 +0000

    1. modify the account table. 2. use the account table to verify login

--- RecipeProject/src/main/resources/DDL_ACCOUNT.sql
+++ RecipeProject/src/main/resources/DDL_ACCOUNT.sql
@@ -1,14 +1,34 @@
+
+DROP TABLE IF EXISTS authorities;
 DROP TABLE IF EXISTS Account;
 
 CREATE  TABLE Account (
 
   email VARCHAR(45) NOT NULL ,
   password VARCHAR(45) NOT NULL ,
-  userName VARCHAR(45) NOT NULL ,
+  username VARCHAR(45) NOT NULL ,
   picture LONGBLOB NULL ,
   bio BLOB NULL ,
   location VARCHAR(45) NULL ,
   twitter VARCHAR(45) NULL ,
+  enabled TINYINT NULL ,
   version int default 0,
   PRIMARY KEY (userName) );
+
+
+CREATE  TABLE authorities (
+
+  username VARCHAR(50) NOT NULL ,
+  authority VARCHAR(50) NULL ,
+  PRIMARY KEY (username) ,
+  
+  CONSTRAINT fk_authorities_users
+    FOREIGN KEY (username)
+    REFERENCES account (username)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
+    
+CREATE INDEX ix_auth_username ON authorities (username ASC, authority ASC);
+CREATE INDEX fk_authorities_users ON authorities (username ASC);
 
