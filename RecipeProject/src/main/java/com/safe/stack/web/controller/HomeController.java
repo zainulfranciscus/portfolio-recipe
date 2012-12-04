9e9919e6430f462cfd12c7f73a09d92911b00dc5
zainul.franciscus@gmail.com <zainul.franciscus@gmail.com@8c8cbd4e-df39-adea-edf0-913d4a500e9e>
2012-12-04 04:39:41 +0000

    use email to authenticate users instead of their user name

--- RecipeProject/src/main/java/com/safe/stack/web/controller/HomeController.java
+++ RecipeProject/src/main/java/com/safe/stack/web/controller/HomeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.safe.stack.domain.Account;
 import com.safe.stack.service.AccountService;
@@ -22,72 +23,76 @@ import com.safe.stack.web.form.Message;
 @Controller
 public class HomeController {
 
-	private static final String RECIPE_LIST_PAGE = "recipe/list";
-	private static final String RECIPE_LOGIN_PAGE = "recipe/login";
+    protected static final String RECIPE_LIST_PAGE = "recipe/list";
+    protected static final String RECIPE_LOGIN_PAGE = "recipe/login";
 
-	@Autowired
-	private MessageSource messageSource;
+    @Autowired
+    private MessageSource messageSource;
 
-	@Autowired
-	private RecipeService recipeService;
+    @Autowired
+    private RecipeService recipeService;
 
-	@Autowired
-	private AccountService accountService;
+    @Autowired
+    private AccountService accountService;
 
-	@RequestMapping(method = RequestMethod.GET)
-	public String showAllRecipes(Model uiModel) {
-		uiModel.addAttribute("recipes", recipeService.findAll());
-		return RECIPE_LIST_PAGE;
+    @RequestMapping(method = RequestMethod.GET)
+    public String showAllRecipes(Model uiModel) {
+	uiModel.addAttribute("recipes", recipeService.findAll());
+	return RECIPE_LIST_PAGE;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String showRecipe(@PathVariable("id") Long id, Model uiModel) {
+	uiModel.addAttribute("recipe", recipeService.findRecipe(id));
+	return "recipe";
+    }
+
+    @RequestMapping(value = "/searchRecipeByIngredient", method = RequestMethod.POST)
+    public String searchRecipes(@RequestParam("ingredient") String ingredient, Model uiModel) {
+	String[] tokens = ingredient.trim().split("\\s+");
+	List<String> ingredients = new ArrayList<String>();
+
+	for (String t : tokens) {
+	    ingredients.add(t);
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public String showRecipe(@PathVariable("id") Long id, Model uiModel) {
-		uiModel.addAttribute("recipe", recipeService.findRecipe(id));
-		return "recipe";
-	}
+	uiModel.addAttribute("recipes", recipeService.findByIngredients(ingredients));
+	return "list";
+    }
 
-	@RequestMapping(value = "/searchRecipeByIngredient", method = RequestMethod.POST)
-	public String searchRecipes(@RequestParam("ingredient") String ingredient,
-			Model uiModel) {
-		String[] tokens = ingredient.trim().split("\\s+");
-		List<String> ingredients = new ArrayList<String>();
+    @RequestMapping(value = "/searchLikedRecipe", method = RequestMethod.POST)
+    public String showLikedRecipe(@RequestParam("userName") String userName, Model uiModel) {
+	Account account = accountService.findByUserName(userName);
+	uiModel.addAttribute("account", account);
+	return "account";
+    }
 
-		for (String t : tokens) {
-			ingredients.add(t);
-		}
+    @RequestMapping(value = "/likeARecipe", method = RequestMethod.POST)
+    public String likeARecipe(@RequestParam("userName") String userName,
+	    @RequestParam("recipeId") String recipeId) {
+	accountService.likeARecipe(userName, Long.parseLong(recipeId));
+	return "recipe";
+    }
 
-		uiModel.addAttribute("recipes",
-				recipeService.findByIngredients(ingredients));
-		return "list";
-	}
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String openLoginPage(Model uiModel) {
+	return RECIPE_LOGIN_PAGE;
+    }
 
-	@RequestMapping(value = "/searchLikedRecipe", method = RequestMethod.POST)
-	public String showLikedRecipe(@RequestParam("userName") String userName,
-			Model uiModel) {
-		Account account = accountService.findByUserName(userName);
-		uiModel.addAttribute("account", account);
-		return "account";
-	}
-
-	@RequestMapping(value = "/likeARecipe", method = RequestMethod.POST)
-	public String likeARecipe(@RequestParam("userName") String userName,
-			@RequestParam("recipeId") String recipeId) {
-		accountService.likeARecipe(userName, Long.parseLong(recipeId));
-		return "recipe";
-	}
-
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String signIn(Model uiModel) {
-		return RECIPE_LOGIN_PAGE;
-	}
-
-	@RequestMapping("/loginfail")
-	public String loginFail(Model uiModel, Locale locale) {
-		uiModel.addAttribute(
-				"login_failed",
-				new Message("error", messageSource.getMessage(
-						"message_login_fail", new Object[] {}, locale)));
-		return RECIPE_LOGIN_PAGE;
-	}
+    @RequestMapping("/loginfail")
+    public String loginFail(Model uiModel, Locale locale) {
+	uiModel.addAttribute(
+		"login_failed",
+		new Message("error", messageSource.getMessage("message_login_fail",
+			new Object[] {}, locale)));
+	return RECIPE_LOGIN_PAGE;
+    }
+    
+    @RequestMapping(value = "/signUp", method = RequestMethod.POST, params="SignUp")
+    public String signUp(Model uiModel){
+	
+	return RECIPE_LIST_PAGE;
+	
+    }
 
 }
---
+++ RecipeProject/src/test/java/com/safe/stack/web/controller/HomeControllerTest.java
@@ -0,0 +1,169 @@
+package com.safe.stack.web.controller;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.context.MessageSource;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.safe.stack.domain.Account;
+import com.safe.stack.domain.Recipe;
+import com.safe.stack.service.AccountService;
+import com.safe.stack.service.RecipeService;
+import com.safe.stack.web.controller.HomeController;
+
+public class HomeControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void testShowAllRecipe() {
+	RecipeService recipeService = mock(RecipeService.class);
+	HomeController homeController = new HomeController();
+
+	List<Recipe> recipeList = new ArrayList<Recipe>();
+	recipeList.add(new Recipe());
+
+	when(recipeService.findAll()).thenReturn(recipeList);
+	ReflectionTestUtils.setField(homeController, "recipeService", recipeService);
+
+	ExtendedModelMap uiModel = new ExtendedModelMap();
+	String result = homeController.showAllRecipes(uiModel);
+
+	assertNotNull(result);
+	assertEquals(result, "recipe/list");
+
+	List<Recipe> modelRecipe = (List<Recipe>) uiModel.get("recipes");
+
+	assertEquals(1, modelRecipe.size());
+    }
+
+    @Test
+    public void testShowRecipe() {
+	RecipeService recipeService = mock(RecipeService.class);
+	HomeController homeController = new HomeController();
+
+	Long id = 1l;
+	Recipe aRecipe = new Recipe();
+	when(recipeService.findRecipe(id)).thenReturn(aRecipe);
+
+	ReflectionTestUtils.setField(homeController, "recipeService", recipeService);
+
+	ExtendedModelMap uiModel = new ExtendedModelMap();
+	String result = homeController.showRecipe(1l, uiModel);
+
+	assertNotNull(result);
+	assertEquals(result, "recipe");
+
+	assertEquals(aRecipe, uiModel.get("recipe"));
+
+    }
+
+    @Test
+    public void testSearchRecipes() {
+
+	String ingredient = "spinach beef";
+
+	List<String> ingredients = new ArrayList<String>();
+	ingredients.add("spinach");
+	ingredients.add("beef");
+
+	Recipe spinachBeefRecipe = new Recipe();
+
+	List<Recipe> recipes = new ArrayList<Recipe>();
+	recipes.add(spinachBeefRecipe);
+
+	RecipeService recipeService = mock(RecipeService.class);
+	when(recipeService.findByIngredients(ingredients)).thenReturn(recipes);
+
+	HomeController homeController = new HomeController();
+	ReflectionTestUtils.setField(homeController, "recipeService", recipeService);
+
+	ExtendedModelMap uiModel = new ExtendedModelMap();
+	String result = homeController.searchRecipes(ingredient, uiModel);
+
+	assertNotNull(result);
+	assertEquals(result, "list");
+
+	assertEquals(recipes, uiModel.get("recipes"));
+
+    }
+
+    @Test
+    public void testShowLikedRecipe() {
+	AccountService accountService = mock(AccountService.class);
+	HomeController homeController = new HomeController();
+
+	String userName = "user1";
+	Set<Recipe> likedRecipe = new HashSet<Recipe>();
+
+	Account account = new Account();
+	account.setUserName(userName);
+	account.setLikedRecipes(likedRecipe);
+
+	when(accountService.findByUserName(userName)).thenReturn(account);
+
+	ReflectionTestUtils.setField(homeController, "accountService", accountService);
+
+	ExtendedModelMap uiModel = new ExtendedModelMap();
+	String result = homeController.showLikedRecipe(userName, uiModel);
+
+	assertNotNull(result);
+	assertEquals(result, "account");
+
+	assertEquals(account, uiModel.get("account"));
+
+    }
+
+    @Test
+    public void testLikeARecipe() {
+	AccountService accountService = mock(AccountService.class);
+	HomeController homeController = new HomeController();
+
+	String userName = "user1";
+	Long recipeId = 1l;
+
+	Recipe recipe = new Recipe();
+	recipe.setId(recipeId);
+
+	ReflectionTestUtils.setField(homeController, "accountService", accountService);
+
+	ExtendedModelMap uiModel = new ExtendedModelMap();
+	String result = homeController.likeARecipe(userName, "1");
+
+	Mockito.verify(accountService).likeARecipe(userName, recipeId);
+
+	assertNotNull(result);
+	assertEquals(result, "recipe");
+
+    }
+
+    @Test
+    public void testLoginFail() {
+	HomeController homeController = new HomeController();
+	ExtendedModelMap uiModel = new ExtendedModelMap();
+
+	MessageSource mockMessageSource = mock(MessageSource.class);
+	when(mockMessageSource.getMessage("message_login_fail", new Object[] {}, Locale.UK))
+		.thenReturn("Invalid user name or password");
+
+	ReflectionTestUtils.setField(homeController, "messageSource", mockMessageSource);
+
+	String result = homeController.loginFail(uiModel, Locale.UK);
+
+	assertNotNull(result);
+	assertEquals(result, HomeController.RECIPE_LOGIN_PAGE);
+    }
+
+}
