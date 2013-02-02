323198bba2f5397a642391da36a2147b1544c068
zainul.franciscus@gmail.com <zainul.franciscus@gmail.com@8c8cbd4e-df39-adea-edf0-913d4a500e9e>
2013-02-02 06:06:35 +0000

    change the relationship between ingredient and ingredient type from one to one to many to one

--- trunk/RecipeProject/src/main/java/com/safe/stack/domain/Ingredient.java
+++ trunk/RecipeProject/src/main/java/com/safe/stack/domain/Ingredient.java
@@ -8,7 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Version;
 import javax.validation.constraints.NotNull;
@@ -93,7 +93,7 @@ public class Ingredient {
     /**
      * @return the ingredientType
      */
-    @OneToOne (cascade = { CascadeType.ALL })
+    @ManyToOne (cascade = { CascadeType.ALL })
     @JoinColumn(name="ingredientType")
     @NotNull(message="{validation.ingredient.NotEmpty.message}")
     public IngredientType getIngredientType() {
