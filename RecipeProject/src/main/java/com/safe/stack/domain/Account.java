6acf3143dae4e8190201212679cc274aa1b5dd36
zainul.franciscus@gmail.com <zainul.franciscus@gmail.com@8c8cbd4e-df39-adea-edf0-913d4a500e9e>
2013-03-29 03:23:01 +0000

    fix sing up problem change error from an id to a class

--- RecipeProject/src/main/java/com/safe/stack/domain/Account.java
+++ RecipeProject/src/main/java/com/safe/stack/domain/Account.java
@@ -25,199 +25,199 @@ import org.hibernate.validator.constraints.NotEmpty;
 @Table(name = "Account")
 public class Account implements Serializable {
 
-    private String email;
-    private String password;
-    private String userName;
-    private Blob picture;
-    private String bio;
-    private String location;
-    private String twitter;
-    private int version;
-    private String authority;
+	private String email;
+	private String password;
+	private String userName;
+	private Blob picture;
+	private String bio;
+	private String location;
+	private String twitter;
+	private int version;
+	private String authority;
 
-    private Set<Recipe> likedRecipes = new HashSet<Recipe>();
+	private Set<Recipe> likedRecipes = new HashSet<Recipe>();
 
-    /**
-     * @return the likedRecipes
-     */
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "LikedRecipe", joinColumns = @JoinColumn(name = "email"), inverseJoinColumns = @JoinColumn(name = "recipeId"))
-    public Set<Recipe> getLikedRecipes() {
-	return likedRecipes;
-    }
+	/**
+	 * @return the likedRecipes
+	 */
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "LikedRecipe", joinColumns = @JoinColumn(name = "email"), inverseJoinColumns = @JoinColumn(name = "recipeId"))
+	public Set<Recipe> getLikedRecipes() {
+		return likedRecipes;
+	}
 
-    /**
-     * @param likedRecipes
-     *            the likedRecipes to set
-     */
-    public void setLikedRecipes(Set<Recipe> likedRecipes) {
-	this.likedRecipes = likedRecipes;
-    }
+	/**
+	 * @param likedRecipes
+	 *            the likedRecipes to set
+	 */
+	public void setLikedRecipes(Set<Recipe> likedRecipes) {
+		this.likedRecipes = likedRecipes;
+	}
 
-    /**
-     * @return the email
-     */
-    @NotEmpty(message = "{validation.email.NotEmpty.message}")
-    @Pattern(regexp = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-		+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$",message="{validation.email.invalid.format}")
-    @Column(name = "email")
-    @Id
-    public String getEmail() {
-	return email;
-    }
+	/**
+	 * @return the email
+	 */
+	@NotEmpty(message = "{validation.email.NotEmpty.message}")
+	@Pattern(regexp = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$", message = "{validation.email.invalid.format}")
+	@Column(name = "email")
+	@Id
+	public String getEmail() {
+		return email;
+	}
 
-    /**
-     * @param email
-     *            the email to set
-     */
-    public void setEmail(String email) {
-	this.email = email;
-    }
+	/**
+	 * @param email
+	 *            the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    /**
-     * @return the password
-     */
-    @NotEmpty(message = "{validation.password.NotEmpty.message}")
-    @Pattern(regexp = "^.*(?=.{6,})(?=.*[a-z])(?=.*[A-Z])(?=.*[\\d\\W]).*$", message="{validation.password.invalid.format}")
-    @Column(name = "password")
-    public String getPassword() {
-	return password;
-    }
+	/**
+	 * @return the password
+	 */
+	@NotEmpty(message = "{validation.password.NotEmpty.message}")
+	@Pattern(regexp = "^.*(?=.{6,})(?=.*[a-z])(?=.*[A-Z])(?=.*[\\d\\W]).*$", message = "{validation.password.invalid.format}")
+	@Column(name = "password")
+	public String getPassword() {
+		return password;
+	}
 
-    /**
-     * @param password
-     *            the password to set
-     */
-    public void setPassword(String password) {
-	this.password = password;
-    }
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    /**
-     * @return the userName
-     */    
-    @Column(name = "userName")
-    public String getUserName() {
-	return userName;
-    }
+	/**
+	 * @return the userName
+	 */
+	@Column(name = "userName")
+	public String getUserName() {
+		return userName;
+	}
 
-    /**
-     * @param userName
-     *            the userName to set
-     */
-    public void setUserName(String userName) {
-	this.userName = userName;
-    }
+	/**
+	 * @param userName
+	 *            the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
-    /**
-     * @return the picture
-     */
-    @Column(name = "picture")
-    public Blob getPicture() {
-	return picture;
-    }
+	/**
+	 * @return the picture
+	 */
+	@Column(name = "picture")
+	public Blob getPicture() {
+		return picture;
+	}
 
-    /**
-     * @param picture
-     *            the picture to set
-     */
-    public void setPicture(Blob picture) {
-	this.picture = picture;
-    }
+	/**
+	 * @param picture
+	 *            the picture to set
+	 */
+	public void setPicture(Blob picture) {
+		this.picture = picture;
+	}
 
-    /**
-     * @return the bio
-     */
-    @Column(name = "bio")
-    public String getBio() {
-	return bio;
-    }
+	/**
+	 * @return the bio
+	 */
+	@Column(name = "bio")
+	public String getBio() {
+		return bio;
+	}
 
-    /**
-     * @param bio
-     *            the bio to set
-     */
-    public void setBio(String bio) {
-	this.bio = bio;
-    }
+	/**
+	 * @param bio
+	 *            the bio to set
+	 */
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
 
-    /**
-     * @return the location
-     */
-    @Column(name = "location")
-    public String getLocation() {
-	return location;
-    }
+	/**
+	 * @return the location
+	 */
+	@Column(name = "location")
+	public String getLocation() {
+		return location;
+	}
 
-    /**
-     * @param location
-     *            the location to set
-     */
-    public void setLocation(String location) {
-	this.location = location;
-    }
+	/**
+	 * @param location
+	 *            the location to set
+	 */
+	public void setLocation(String location) {
+		this.location = location;
+	}
 
-    /**
-     * @return the twitter
-     */
-    @Column(name = "twitter")
-    public String getTwitter() {
-	return twitter;
-    }
+	/**
+	 * @return the twitter
+	 */
+	@Column(name = "twitter")
+	public String getTwitter() {
+		return twitter;
+	}
 
-    /**
-     * @param twitter
-     *            the twitter to set
-     */
-    public void setTwitter(String twitter) {
-	this.twitter = twitter;
-    }
+	/**
+	 * @param twitter
+	 *            the twitter to set
+	 */
+	public void setTwitter(String twitter) {
+		this.twitter = twitter;
+	}
 
-    /**
-     * @return the version
-     */
-    @Version
-    @Column(name = "version")
-    public int getVersion() {
-	return version;
-    }
+	/**
+	 * @return the version
+	 */
+	@Version
+	@Column(name = "version")
+	public int getVersion() {
+		return version;
+	}
 
-    /**
-     * @param version
-     *            the version to set
-     */
-    public void setVersion(int version) {
-	this.version = version;
-    }
+	/**
+	 * @param version
+	 *            the version to set
+	 */
+	public void setVersion(int version) {
+		this.version = version;
+	}
 
-    /**
-     * @return the authority
-     */
-    @Column(name = "authority")
-    public String getAuthority() {
-        return authority;
-    }
+	/**
+	 * @return the authority
+	 */
+	@Column(name = "authority")
+	public String getAuthority() {
+		return authority;
+	}
 
-    /**
-     * @param authority the authority to set
-     */
-    public void setAuthority(String authority) {
-        this.authority = authority;
-    }
-    
-    /**
-     * @param userName of a user
-     * @param password of a user
-     * @return a new Account that can be used to create a new user account
-     */
-    public static Account getNewAccount(String userName, String password){
-	Account acc = new Account();
-	acc.setEmail(userName);
-	acc.setPassword(password);
-	acc.setUserName("user" + userName.hashCode());
-	acc.setAuthority("user");
-	return acc;
-    }
-    
-    
+	/**
+	 * @param authority
+	 *            the authority to set
+	 */
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+	/**
+	 * @param userName
+	 *            of a user
+	 * @param password
+	 *            of a user
+	 * @return a new Account that can be used to create a new user account
+	 */
+	public static Account getNewAccount(String userName, String password) {
+		Account acc = new Account();
+		acc.setEmail(userName);
+		acc.setPassword(password);
+		acc.setUserName("user" + userName.hashCode());
+		acc.setAuthority("user");
+		return acc;
+	}
 
 }
