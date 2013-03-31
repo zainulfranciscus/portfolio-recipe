c600ffa558bc0005ebb292f50fefb32ba2d0e527
zainul.franciscus@gmail.com <zainul.franciscus@gmail.com@8c8cbd4e-df39-adea-edf0-913d4a500e9e>
2013-03-31 11:21:58 +0000

    For a webservice client to login to the website

---
+++ trunk/RecipeProject/src/test/java/com/safe/stack/web/controller/CustomCredentialsProvider.java
@@ -0,0 +1,13 @@
+package com.safe.stack.web.controller;
+
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+
+public class CustomCredentialsProvider extends BasicCredentialsProvider {
+
+	public void setCredentials(Credentials credentials) {
+		super.setCredentials(AuthScope.ANY, credentials);
+	}
+
+}
