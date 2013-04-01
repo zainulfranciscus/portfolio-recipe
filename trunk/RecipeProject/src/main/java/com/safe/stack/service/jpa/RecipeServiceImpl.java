279d6d8278662056d4f37ceddf1308f2223270d6
zainul.franciscus@gmail.com <zainul.franciscus@gmail.com@8c8cbd4e-df39-adea-edf0-913d4a500e9e>
2013-04-01 00:54:33 +0000

    add documentation for this class

--- trunk/RecipeProject/src/main/java/com/safe/stack/service/jpa/RecipeServiceImpl.java
+++ trunk/RecipeProject/src/main/java/com/safe/stack/service/jpa/RecipeServiceImpl.java
@@ -2,8 +2,6 @@ package com.safe.stack.service.jpa;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,197 +26,227 @@ import com.safe.stack.domain.RecipeSummary;
 import com.safe.stack.repository.RecipeRepository;
 import com.safe.stack.service.RecipeService;
 
+/**
+ * A service class responsible for retrieving information about a recipe from
+ * the database
+ * 
+ * @author Zainul Franciscus
+ * 
+ */
 @Service("recipeService")
 @Repository
 @Transactional
 public class RecipeServiceImpl implements RecipeService {
 
-    @Autowired
-    private RecipeRepository recipeRepository;
+	/**
+	 * Used to save recipe to the database
+	 */
+	@Autowired
+	private RecipeRepository recipeRepository;
 
-    @PersistenceContext
-    private EntityManager entityManager;
+	/**
+	 * Used to retrieve recipe from the database
+	 */
+	@PersistenceContext
+	private EntityManager entityManager;
 
-    private static final String FIND_RECIPE_BY_INGREDIENT_SQL = "select distinct(r) from Recipe as r join r.ingredients as i join i.ingredientType as t where ";
+	/**
+	 * An SQL used to search recipes based on a set of ingredients
+	 */
+	private static final String FIND_RECIPE_BY_INGREDIENT_SQL = "select distinct(r) from Recipe as r join r.ingredients as i join i.ingredientType as t where ";
 
-    private static final String NATIVEQUERY_RECIPES_WITH_NUM_OF_LIKES = "select new com.safe.stack.domain.RecipeSummary(r.id, r.name, r.author, r.diet, "
-	    + "(select count(*) from LikedRecipe l where r.id = l.recipeId) as numOfLikes, r.authorLink, r.picture) "
-	    + "from Recipe r";
+	/**
+	 * Retrieve a list of recipe with the number of likes per recipe. 
+	 * This SQL is intended to produce a list of RecipeSummary objects
+	 */
+	private static final String NATIVEQUERY_RECIPES_WITH_NUM_OF_LIKES = "select new com.safe.stack.domain.RecipeSummary(r.id, r.name, r.author, r.diet, "
+			+ "(select count(*) from LikedRecipe l where r.id = l.recipeId) as numOfLikes, r.authorLink, r.picture) " + "from Recipe r";
 
-    private static final String NATIVEQUERY_RECIPES_WITH_LIKED_INDICATOR = "select new com.safe.stack.domain.RecipeSummary(r.id, r.name, r.author, r.diet, "
-	    + "(select count(*) from LikedRecipe l where r.id = l.recipeId) as numOfLikes, r.authorLink, r.picture,"
-	    + "(select count(*) from LikedRecipe l where r.id = l.recipeId and l.email =:arg0) as likedByUser) "
-	    + "from Recipe r";
+	/**
+	 * Retrieve a list of recipes, where each recipe has information whether a logged in user
+	 * has liked that recipe.
+	 * This SQL is intended to produce a list of RecipeSummary Objects.
+	 */
+	private static final String NATIVEQUERY_RECIPES_WITH_LIKED_INDICATOR = "select new com.safe.stack.domain.RecipeSummary(r.id, r.name, r.author, r.diet, "
+			+ "(select count(*) from LikedRecipe l where r.id = l.recipeId) as numOfLikes, r.authorLink, r.picture,"
+			+ "(select count(*) from LikedRecipe l where r.id = l.recipeId and l.email =:arg0) as likedByUser) " + "from Recipe r";
 
-    private static final int NAME_COL = 0;
+	private static final int NAME_COL = 0;
 
-    private static final int AUTHOR_COL = 1;
+	private static final int AUTHOR_COL = 1;
 
-    private static final int DIET_COL = 2;
+	private static final int DIET_COL = 2;
 
-    private static final int AUTHOR_LINK_COL = 3;
+	private static final int AUTHOR_LINK_COL = 3;
 
-    private static final int PICTURE_COL = 4;
+	private static final int PICTURE_COL = 4;
 
-    private static final int INGREDIENT_COL = 5;
+	private static final int INGREDIENT_COL = 5;
 
-    private static final int AMOUNT_COL = 6;
+	private static final int AMOUNT_COL = 6;
 
-    private static final int METRIC_COL = 7;
+	private static final int METRIC_COL = 7;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * com.safe.stack.service.RecipeService#save(com.safe.stack.domain.Recipe)
-     */
-    public void save(Recipe recipe) {
-	entityManager.merge(recipe);
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.safe.stack.service.RecipeService#findRecipe(java.lang.Long)
-     */
-    @Transactional(readOnly=true)
-    public Recipe findRecipe(Long id) {
-	return recipeRepository.findOne(id);
-    }
-
-    @Transactional(readOnly=true)
-    public List<Recipe> findAll() {
-	return (List<Recipe>) entityManager.createNamedQuery("Recipe.findAll", Recipe.class)
-		.getResultList();
-    }
-
-    @Transactional(readOnly=true)
-    public List<IngredientType> findAllIngredientTypes() {
-	return (List<IngredientType>) entityManager.createNamedQuery("IngredientType.findAll",
-		IngredientType.class).getResultList();
-    }
-
-    @Transactional(readOnly=true)
-    public List<Recipe> findByIngredients(List<String> ingredients) {
-
-	if (ingredients.size() == 0) {
-	    return new ArrayList<Recipe>();
+	/*
+	 * Save the supplied recipe object into the database
+	 * 
+	 * @see
+	 * com.safe.stack.service.RecipeService#save(com.safe.stack.domain.Recipe)
+	 */
+	public void save(Recipe recipe) {
+		entityManager.merge(recipe);
 	}
 
-	StringBuilder builder = new StringBuilder();
-	builder.append(FIND_RECIPE_BY_INGREDIENT_SQL);
-
-	for (int i = 0; i < ingredients.size(); i++) {
-	    if (i == 0) {
-		builder.append("t.name LIKE :arg0");
-		continue;
-	    }
-
-	    builder.append(" or t.name LIKE :arg").append(i);
-	}
-
-	Query query = entityManager.createQuery(builder.toString());
-	for (int i = 0; i < ingredients.size(); i++) {
-	    query.setParameter("arg" + i, "%" + ingredients.get(i) + "%");
-	}
-	return query.getResultList();
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.safe.stack.service.RecipeService#findRecipesWithNumOfLikes()
-     */
-    @Override
-    @Transactional(readOnly=true)
-    public List<RecipeSummary> findRecipesWithNumOfLikes() {
-
-	return entityManager.createQuery(NATIVEQUERY_RECIPES_WITH_NUM_OF_LIKES).getResultList();
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * com.safe.stack.service.RecipeService#findRecipesWithLlikedIndicator(java
-     * .lang.String)
-     */
-    @Override
-    @Transactional(readOnly=true)
-    public List<RecipeSummary> findRecipesWithLlikedIndicator(String userName) {
-	Query q = entityManager.createQuery(NATIVEQUERY_RECIPES_WITH_LIKED_INDICATOR);
-	q.setParameter("arg0", userName);
-	return q.getResultList();
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.safe.stack.service.RecipeService#importData()
-     */
-    @Override
-    public Iterable<Recipe> importData(File excel) throws BiffException, IOException {
 	
-	Workbook workbook = Workbook.getWorkbook(excel);
-	Sheet sheet = workbook.getSheet(0);
-	int numOfRow = sheet.getRows();
-
-	List<IngredientType> ingredientTypes = findAllIngredientTypes();
-	List<Recipe> recipeList = new ArrayList<Recipe>();
-
-	for (int i = 1; i < numOfRow; i++) {
-
-	    String recipeName = sheet.getCell(NAME_COL, i).getContents();
-	    Recipe r = new Recipe();
-
-	    if (!StringUtils.isEmpty(recipeName)) {
-		String authorName = sheet.getCell(AUTHOR_COL, i).getContents().trim();
-		String recipePicture = sheet.getCell(PICTURE_COL, i).getContents().trim();
-		String authorURL = sheet.getCell(AUTHOR_LINK_COL, i).getContents().trim();
-		String diet = sheet.getCell(DIET_COL, i).getContents().trim();
-
-		r.setAuthor(authorName);
-		r.setAuthorLink(authorURL);
-		r.setDiet(diet);
-
-		r.setName(recipeName);
-		r.setPicture(recipePicture);
-
-	    } else {
-		r = recipeList.get(recipeList.size() - 1);
-	    }
-
-	    String ingredientName = sheet.getCell(INGREDIENT_COL, i).getContents().trim();
-	    String ingredientAmt = sheet.getCell(AMOUNT_COL, i).getContents().trim();
-	    String ingredientMetric = sheet.getCell(METRIC_COL, i).getContents().trim();
-
-	    Ingredient ingr = new Ingredient();
-	    ingr.setAmount(ingredientAmt);
-	    ingr.setMetric(ingredientMetric);
-
-	    IngredientType t = new IngredientType();
-	    t.setName(ingredientName);
-
-	    for (IngredientType type : ingredientTypes) {
-		if (type.getName().equalsIgnoreCase(ingredientName))		    
-		{
-		    t = type;
-		    break;
-		}
-	    }
-
-	    ingr.setIngredientType(t);
-
-	    r.getIngredients().add(ingr);
-
-	    if (!StringUtils.isEmpty(recipeName)) {
-		recipeList.add(r);
-	    }
-
+	/* Find a recipe with the supplied recipe id.
+	 * @see com.safe.stack.service.RecipeService#findRecipe(java.lang.Long)
+	 */
+	@Transactional(readOnly = true)
+	public Recipe findRecipe(Long id) {
+		return recipeRepository.findOne(id);
 	}
 
-	return recipeRepository.save(recipeList);
+	/* Retrieve all recipes in the database
+	 * @see com.safe.stack.service.RecipeService#findAll()
+	 */
+	@Transactional(readOnly = true)
+	public List<Recipe> findAll() {
+		return (List<Recipe>) entityManager.createNamedQuery("Recipe.findAll", Recipe.class).getResultList();
+	}
 
-    }
+	/* Retrieve every ingredients stored in the database.
+	 * @see com.safe.stack.service.RecipeService#findAllIngredientTypes()
+	 */
+	@Transactional(readOnly = true)
+	public List<IngredientType> findAllIngredientTypes() {
+		return (List<IngredientType>) entityManager.createNamedQuery("IngredientType.findAll", IngredientType.class).getResultList();
+	}
+
+	/* Retrieve a list of recipes based on the supplied ingredients
+	 * @see com.safe.stack.service.RecipeService#findByIngredients(java.util.List)
+	 */
+	@Transactional(readOnly = true)
+	public List<Recipe> findByIngredients(List<String> ingredients) {
+
+		if (ingredients.size() == 0) {
+			return new ArrayList<Recipe>();
+		}
+
+		StringBuilder builder = new StringBuilder();
+		builder.append(FIND_RECIPE_BY_INGREDIENT_SQL);
+
+		for (int i = 0; i < ingredients.size(); i++) {
+			if (i == 0) {
+				builder.append("t.name LIKE :arg0");
+				continue;
+			}
+
+			builder.append(" or t.name LIKE :arg").append(i);
+		}
+
+		Query query = entityManager.createQuery(builder.toString());
+		for (int i = 0; i < ingredients.size(); i++) {
+			query.setParameter("arg" + i, "%" + ingredients.get(i) + "%");
+		}
+		return query.getResultList();
+	}
+
+	/*
+	 * Retrieve a list of RecipeSummary objects where each object stores the number of 
+	 * user who like each of that recipe.
+	 * 
+	 * @see com.safe.stack.service.RecipeService#findRecipesWithNumOfLikes()
+	 */
+	@Override
+	@Transactional(readOnly = true)
+	public List<RecipeSummary> findRecipesWithNumOfLikes() {
+
+		return entityManager.createQuery(NATIVEQUERY_RECIPES_WITH_NUM_OF_LIKES).getResultList();
+	}
+
+	/*
+	 * Return a list of RecipeSummary objects that have information whether
+	 * a user has liked each summary
+	 * 
+	 * @see
+	 * com.safe.stack.service.RecipeService#findRecipesWithLlikedIndicator(java
+	 * .lang.String)
+	 */
+	@Override
+	@Transactional(readOnly = true)
+	public List<RecipeSummary> findRecipesWithLlikedIndicator(String userName) {
+		Query q = entityManager.createQuery(NATIVEQUERY_RECIPES_WITH_LIKED_INDICATOR);
+		q.setParameter("arg0", userName);
+		return q.getResultList();
+	}
+
+	/*
+	 * Create recipes based on the information recorded in the provided excel spreadsheet.
+	 * 
+	 * @see com.safe.stack.service.RecipeService#importData()
+	 */
+	@Override
+	public Iterable<Recipe> importData(File excel) throws BiffException, IOException {
+
+		Workbook workbook = Workbook.getWorkbook(excel);
+		Sheet sheet = workbook.getSheet(0);
+		int numOfRow = sheet.getRows();
+
+		List<IngredientType> ingredientTypes = findAllIngredientTypes();
+		List<Recipe> recipeList = new ArrayList<Recipe>();
+
+		for (int i = 1; i < numOfRow; i++) {
+
+			String recipeName = sheet.getCell(NAME_COL, i).getContents();
+			Recipe r = new Recipe();
+
+			if (!StringUtils.isEmpty(recipeName)) {
+				String authorName = sheet.getCell(AUTHOR_COL, i).getContents().trim();
+				String recipePicture = sheet.getCell(PICTURE_COL, i).getContents().trim();
+				String authorURL = sheet.getCell(AUTHOR_LINK_COL, i).getContents().trim();
+				String diet = sheet.getCell(DIET_COL, i).getContents().trim();
+
+				r.setAuthor(authorName);
+				r.setAuthorLink(authorURL);
+				r.setDiet(diet);
+
+				r.setName(recipeName);
+				r.setPicture(recipePicture);
+
+			} else {
+				r = recipeList.get(recipeList.size() - 1);
+			}
+
+			String ingredientName = sheet.getCell(INGREDIENT_COL, i).getContents().trim();
+			String ingredientAmt = sheet.getCell(AMOUNT_COL, i).getContents().trim();
+			String ingredientMetric = sheet.getCell(METRIC_COL, i).getContents().trim();
+
+			Ingredient ingr = new Ingredient();
+			ingr.setAmount(ingredientAmt);
+			ingr.setMetric(ingredientMetric);
+
+			IngredientType t = new IngredientType();
+			t.setName(ingredientName);
+
+			for (IngredientType type : ingredientTypes) {
+				if (type.getName().equalsIgnoreCase(ingredientName)) {
+					t = type;
+					break;
+				}
+			}
+
+			ingr.setIngredientType(t);
+
+			r.getIngredients().add(ingr);
+
+			if (!StringUtils.isEmpty(recipeName)) {
+				recipeList.add(r);
+			}
+
+		}
+
+		return recipeRepository.save(recipeList);
+
+	}
 
 }
