9f6902f1fc09f7c9634e8741e7e7e1961535158a
zainul.franciscus@gmail.com <zainul.franciscus@gmail.com@8c8cbd4e-df39-adea-edf0-913d4a500e9e>
2013-04-02 06:26:33 +0000

    remove bean definition from the xml file, since the bean is autowired using annotations

--- trunk/RecipeProject/src/main/java/com/safe/stack/service/RecipeUploader.java
+++ trunk/RecipeProject/src/main/java/com/safe/stack/service/RecipeUploader.java
@@ -6,19 +6,25 @@ import jxl.read.biff.BiffException;
 
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+/**
+ * This class save a recipes into the database. Data for the recipes are stored in an excel
+ * spredsheet.
+ * @author Zainul Franciscus
+ *
+ */
 public class RecipeUploader {
 
-    public static void main(String[] args) throws BiffException, IOException {
+	public static void main(String[] args) throws BiffException, IOException {
 
-	System.setProperty("spring.profiles.active", "mysql");
+		System.setProperty("spring.profiles.active", "mysql");
 
-	GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
-	ctx.load("classpath:jpa-app-context.xml","classpath:security-context.xml","classpath:datasource-tx-jpa.xml");
-	ctx.refresh();
-	
-	RecipeService recipeService = ctx.getBean("recipeService", RecipeService.class);
-	recipeService.importData();
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.load("classpath:jpa-app-context.xml", "classpath:security-context.xml", "classpath:datasource-tx-jpa.xml");
+		ctx.refresh();
 
-    }
+		RecipeService recipeService = ctx.getBean("recipeService", RecipeService.class);
+		recipeService.importData();
+
+	}
 
 }
