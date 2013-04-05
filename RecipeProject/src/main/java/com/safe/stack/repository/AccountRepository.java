448bdcd0a0a066d5509a30e5f9f4a81c523229fb
zainul.franciscus@gmail.com <zainul.franciscus@gmail.com@8c8cbd4e-df39-adea-edf0-913d4a500e9e>
2013-04-05 02:37:50 +0000

    Add documentations

--- RecipeProject/src/main/java/com/safe/stack/repository/AccountRepository.java
+++ RecipeProject/src/main/java/com/safe/stack/repository/AccountRepository.java
@@ -4,9 +4,28 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.safe.stack.domain.Account;
 
+/**
+ * An implementation of  CrudRepository intended to provide CRUD operations 
+ * for Account table
+ * 
+ * @author Zainul Franciscus
+ *
+ */
 public interface AccountRepository extends CrudRepository<Account, Long>{
 	
+	/**
+	 * Find an account based on the provided email
+	 * 
+	 * @param email of a user
+	 * @return An account object that matches the provided email.
+	 */
 	Account findByEmail(String email);
+	/**
+	 * Find an account based on the provided email and user name
+	 * @param email of a user
+	 * @param userName of a user
+	 * @return an Account object that matches the provided email and username
+	 */
 	Account findByEmailAndUserName(String email, String userName);	
 
 }
--- RecipeProject/src/main/java/com/safe/stack/repository/LikedRecipeRepository.java
+++ RecipeProject/src/main/java/com/safe/stack/repository/LikedRecipeRepository.java
@@ -4,7 +4,13 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.safe.stack.domain.LikedRecipe;
 
-public interface LikedRecipeRepository extends
-		CrudRepository<LikedRecipe, Long> {
+/**
+ * An implementation of CrudRepository interface intended to provide CRUD operation 
+ * for LikedRecipe table
+ * 
+ * @author Zainul Franciscus
+ * 
+ */
+public interface LikedRecipeRepository extends CrudRepository<LikedRecipe, Long> {
 
 }
