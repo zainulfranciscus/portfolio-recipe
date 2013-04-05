cd4f1b785beb17c162efccd5bf7381305d063bf0
zainul.franciscus@gmail.com <zainul.franciscus@gmail.com@8c8cbd4e-df39-adea-edf0-913d4a500e9e>
2013-04-05 02:37:50 +0000

    Add documentations

--- trunk/RecipeProject/src/main/java/com/safe/stack/repository/RecipeRepository.java
+++ trunk/RecipeProject/src/main/java/com/safe/stack/repository/RecipeRepository.java
@@ -4,6 +4,13 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.safe.stack.domain.Recipe;
 
+/**
+ * An implementation of CrudRepository intended to provide CRUD operation 
+ * for the Recipe table.
+ * 
+ * @author Zainul Franciscus
+ *
+ */
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 
 }
