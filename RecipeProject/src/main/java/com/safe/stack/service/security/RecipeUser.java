82fbbe2af8a9af8a5dea0947556c7a651a39cdd6
zainul.franciscus@gmail.com <zainul.franciscus@gmail.com@8c8cbd4e-df39-adea-edf0-913d4a500e9e>
2013-04-05 03:26:06 +0000

    add documentations

--- RecipeProject/src/main/java/com/safe/stack/service/security/RecipeUser.java
+++ RecipeProject/src/main/java/com/safe/stack/service/security/RecipeUser.java
@@ -4,14 +4,22 @@ import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 
+/**
+ * A User of this website has a user has an alias, that 
+ * might be different than the user's user name. To support alias, a custom type of a User class has to be created,
+ * and this is the purpose of this class.
+ * 
+ * @author Zainul Franciscus
+ *
+ */
 public class RecipeUser extends User {
 
+    /**
+     * An alias of a user.
+     */
     private String userAlias;
-    
-   
-
+ 
     public RecipeUser(String username, String password, boolean enabled, boolean accountNonExpired,
 	    boolean credentialsNonExpired, boolean accountNonLocked,
 	    Collection<? extends GrantedAuthority> authorities) {
