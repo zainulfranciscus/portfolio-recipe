8299bf550e680cbbe8426d7610ba8a7baf3cc380
zainul.franciscus@gmail.com <zainul.franciscus@gmail.com@8c8cbd4e-df39-adea-edf0-913d4a500e9e>
2013-04-05 02:44:25 +0000

    Add documentations

--- trunk/RecipeProject/src/main/java/com/safe/stack/service/RecipeService.java
+++ trunk/RecipeProject/src/main/java/com/safe/stack/service/RecipeService.java
@@ -1,31 +1,72 @@
 package com.safe.stack.service;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
-
-import jxl.read.biff.BiffException;
 
 import com.safe.stack.domain.IngredientType;
 import com.safe.stack.domain.Recipe;
 import com.safe.stack.domain.RecipeSummary;
 
+/**
+ * An interface that provides method that can be used to save a recipes or find
+ * information related to a recipe. For example: finding a recipe based on a a
+ * list of ingredients
+ * 
+ * @author Zainul Franciscus
+ * 
+ */
 public interface RecipeService {
 
-    void save(Recipe recipe);
+	/**
+	 * Persist the provided recipe object into a database.
+	 * 
+	 * @param recipe 
+	 */
+	void save(Recipe recipe);
 
-    Recipe findRecipe(Long id);
+	/**
+	 * Find a recipe based on a recipe id.
+	 * @param id of a recipe
+	 * @return a recipe object
+	 */
+	Recipe findRecipe(Long id);
 
-    List<Recipe> findAll();
+	/**
+	 * @return a list of recipes
+	 */
+	List<Recipe> findAll();
 
-    List<Recipe> findByIngredients(List<String> ingredients);
+	/**
+	 * @param ingredients
+	 * @return a list of recipe with ingredients that match the provided
+	 * list of ingredients.
+	 */
+	List<Recipe> findByIngredients(List<String> ingredients);
 
-    List<IngredientType> findAllIngredientTypes();
-    
-    List<RecipeSummary> findRecipesWithNumOfLikes();
-    
-    List<RecipeSummary> findRecipesWithLlikedIndicator(String userName);
-    
-    Iterable<Recipe> importData();
+	/**
+	 * @return a list of IngredientType
+	 */
+	List<IngredientType> findAllIngredientTypes();
+
+	/**
+	 * Find recipes with information on the number of users
+	 * who have liked each recipe.
+	 * 
+	 * @return a list of recipe summary of objects
+	 */
+	List<RecipeSummary> findRecipesWithNumOfLikes();
+
+	/**
+	 * Find recipes with information whether a user, who has the provided user name, 
+	 * has like each recipe.
+	 * 
+	 * @param userName of a user
+	 * @return a list of RecipeSummary objects
+	 */
+	List<RecipeSummary> findRecipesWithLlikedIndicator(String userName);
+
+	/**
+	 * @return a list of recipes that has been inserted into the database
+	 */
+	Iterable<Recipe> importData();
 
 }
