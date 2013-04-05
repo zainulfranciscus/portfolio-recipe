f5e3b3f748cbb3cf2e7daad1ce9f0e0e6092ff82
zainul.franciscus@gmail.com <zainul.franciscus@gmail.com@8c8cbd4e-df39-adea-edf0-913d4a500e9e>
2013-04-05 03:26:06 +0000

    add documentations

--- trunk/RecipeProject/src/main/java/com/safe/stack/service/security/SuccessHandler.java
+++ trunk/RecipeProject/src/main/java/com/safe/stack/service/security/SuccessHandler.java
@@ -10,31 +10,38 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
+/**
+ * A user could sign in from more than 1 page in this application. Once they have signed in, 
+ * users must be redirected to a page where the click the sign in button. 
+ * To do this, the application must obtain the URL where users click the sign-in button, and use
+ * that URL to redirect user to the URL where users click the sign-in button. This is the 
+ * purpose of this class.
+ * 
+ * @author Zainul Franciscus
+ *
+ */
 public class SuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.springframework.security.web.authentication.
-     * SavedRequestAwareAuthenticationSuccessHandler
-     * #onAuthenticationSuccess(javax.servlet.http.HttpServletRequest,
-     * javax.servlet.http.HttpServletResponse,
-     * org.springframework.security.core.Authentication)
-     */
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-	    Authentication authentication) throws ServletException, IOException {
+	/*
+	 * Redirect user to a URL once they sign in.
+	 * 
+	 * @see org.springframework.security.web.authentication.
+	 * SavedRequestAwareAuthenticationSuccessHandler
+	 * #onAuthenticationSuccess(javax.servlet.http.HttpServletRequest,
+	 * javax.servlet.http.HttpServletResponse,
+	 * org.springframework.security.core.Authentication)
+	 */
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
 
-	
-	String url = (String) request.getSession().getAttribute("url_prior_login");
-	
-	Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-	if (principal != null && principal instanceof RecipeUser) {
-	    request.getSession().setAttribute("RecipeUser", (RecipeUser) principal);
+		String url = (String) request.getSession().getAttribute("url_prior_login");
+
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		if (principal != null && principal instanceof RecipeUser) {
+			request.getSession().setAttribute("RecipeUser", (RecipeUser) principal);
+		}
+
+		getRedirectStrategy().sendRedirect(request, response, url);
 	}
-
-	
-	getRedirectStrategy().sendRedirect(request, response, url);
-    }
 
 }
--- trunk/RecipeProject/src/main/java/com/safe/stack/service/security/UserService.java
+++ trunk/RecipeProject/src/main/java/com/safe/stack/service/security/UserService.java
@@ -10,69 +10,77 @@ import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 
+/**
+ * This class is used as part of the Authentication process of this project.
+ * The method defined in this method was created to load a user based on the email
+ * address of the user.
+ * 
+ * @author Zainul Franciscus
+ *
+ */
 public class UserService extends JdbcDaoImpl {
 
-    private String usersByUsernameQuery = "select email,password,enabled,username from account where email = ?";
+	/**
+	 * Find a user based on an email address
+	 */
+	private String usersByUsernameQuery = "select email,password,enabled,username from account where email = ?";
 
-    private static final String DEF_AUTHORITIES_BY_USERNAME_QUERY = "select email, authority from account where email=?  ";
+	/**
+	 * a query used to determine user authorities based on the provided email address.
+	 */
+	private static final String DEF_AUTHORITIES_BY_USERNAME_QUERY = "select email, authority from account where email=?  ";
 
-    public UserService() {
-	super();
-	setAuthoritiesByUsernameQuery(DEF_AUTHORITIES_BY_USERNAME_QUERY);
-    }
+	public UserService() {
+		super();
+		setAuthoritiesByUsernameQuery(DEF_AUTHORITIES_BY_USERNAME_QUERY);
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl#
-     * loadUsersByUsername(java.lang.String)
-     */
-    @Override
-    protected List<UserDetails> loadUsersByUsername(String username) {
+	/*
+	 * Obtain a list of user details based on the provided user name.
+	 * 
+	 * @see org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl#
+	 * loadUsersByUsername(java.lang.String)
+	 */
+	@Override
+	protected List<UserDetails> loadUsersByUsername(String username) {
 
-	return getJdbcTemplate().query(usersByUsernameQuery, new String[] { username },
-		new RowMapper<UserDetails>() {
-		    public UserDetails mapRow(ResultSet rs, int rowNum) throws SQLException {
-			String username = rs.getString(1);
-			String password = rs.getString(2);
-			boolean enabled = rs.getBoolean(3);
-			String alias = rs.getString(4);
+		return getJdbcTemplate().query(usersByUsernameQuery, new String[] { username }, new RowMapper<UserDetails>() {
+			public UserDetails mapRow(ResultSet rs, int rowNum) throws SQLException {
+				String username = rs.getString(1);
+				String password = rs.getString(2);
+				boolean enabled = rs.getBoolean(3);
+				String alias = rs.getString(4);
 
-			RecipeUser recipeUser = new RecipeUser(username, password, enabled, true,
-				true, true, AuthorityUtils.NO_AUTHORITIES);
-			recipeUser.setUserAlias(alias);
+				RecipeUser recipeUser = new RecipeUser(username, password, enabled, true, true, true, AuthorityUtils.NO_AUTHORITIES);
+				recipeUser.setUserAlias(alias);
 
-			return recipeUser;
-		    }
+				return recipeUser;
+			}
 
 		});
-    }
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl#
-     * createUserDetails(java.lang.String,
-     * org.springframework.security.core.userdetails.UserDetails,
-     * java.util.List)
-     */
-    @Override
-    protected UserDetails createUserDetails(String username, UserDetails userFromUserQuery,
-	    List<GrantedAuthority> combinedAuthorities) {
+	/*
+	 * create a user detail based on the provided user name, and authorities.
+	 * 
+	 * @see org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl#
+	 * createUserDetails(java.lang.String,
+	 * org.springframework.security.core.userdetails.UserDetails,
+	 * java.util.List)
+	 */
+	@Override
+	protected UserDetails createUserDetails(String username, UserDetails userFromUserQuery, List<GrantedAuthority> combinedAuthorities) {
 
-	RecipeUser recipeUserFromQuery = (RecipeUser) userFromUserQuery;
+		RecipeUser recipeUserFromQuery = (RecipeUser) userFromUserQuery;
 
-	UserDetails userDetails = super.createUserDetails(username, userFromUserQuery,
-		combinedAuthorities);
+		UserDetails userDetails = super.createUserDetails(username, userFromUserQuery, combinedAuthorities);
 
-	RecipeUser recipeUser = new RecipeUser(userDetails.getUsername(),
-		userDetails.getPassword(), userDetails.isEnabled(),
-		userDetails.isAccountNonExpired(), userDetails.isCredentialsNonExpired(),
-		userDetails.isAccountNonLocked(), userDetails.getAuthorities());
-	
-	recipeUser.setUserAlias(recipeUserFromQuery.getUserAlias());
+		RecipeUser recipeUser = new RecipeUser(userDetails.getUsername(), userDetails.getPassword(), userDetails.isEnabled(), userDetails.isAccountNonExpired(), userDetails.isCredentialsNonExpired(),
+				userDetails.isAccountNonLocked(), userDetails.getAuthorities());
 
-	return recipeUser;
+		recipeUser.setUserAlias(recipeUserFromQuery.getUserAlias());
 
-    }
+		return recipeUser;
+
+	}
 }
